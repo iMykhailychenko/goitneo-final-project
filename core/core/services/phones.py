17738f0eaff8d91ae13ac54b801f56982e54456c
Tetiana Sakun <>
2023-12-19 20:01:37 +0100

    Add validations for birthday, email, phone, name

--- core/core/services/phones.py
+++ core/core/services/phones.py
@@ -1,4 +1,3 @@
-import re
 from core.models import response, Record
 from core.database import write_data, Database
 from core.misc import InfoMessages
@@ -21,13 +20,3 @@ def set_phone_number(payload, record: Record) -> Record:
         return record
     else:
         return Record(name=payload.name, phones={payload.phone})
-
-
-# This code is for the implementation of the US-19
-# def validate_phone_number(phone_number):
-#         pattern = re.compile(r'^[0-9+-]+$')
-
-#         if len(phone_number) != 10:
-#             raise InvalidPhoneLengthError()
-#         elif not pattern.match(phone_number):
-#             raise InvalidPhoneError()
