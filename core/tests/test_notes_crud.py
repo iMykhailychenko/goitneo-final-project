87cf863c48c2a799557eddc2858e4a7b22b9d19d
AlexUtchenko <87366032+AlexUtchenko@users.noreply.github.com>
2023-12-21 22:36:20 +0200

    US-20_26_all_about_tags_1_bug

--- core/tests/test_notes_crud.py
+++ core/tests/test_notes_crud.py
@@ -24,3 +24,46 @@ def test_delete_note(setup_db):
 
 def test_update_note(setup_db):
     pass  # TODO implement after test_get_note
+
+
+def test_add_tags(setup_db):
+    result = controller(Actions.ADD_NOTE, NotePayload(value="test note"))
+    controller(Actions.ADD_TAG, NotePayload(id=result.value.id, tags={"test tag"}))
+    controller(Actions.ADD_TAG, NotePayload(id=result.value.id, tags={"test tag2"}))
+    assert db.select(entity=Entities.NOTES, key=result.value.id).tags == {
+        "test tag",
+        "test tag2",
+    }
+
+
+def test_update_tag(setup_db):
+    result = controller(Actions.ADD_NOTE, NotePayload(value="test note"))
+    controller(Actions.ADD_TAG, NotePayload(id=result.value.id, tags={"test tag"}))
+    controller(Actions.UPDATE_TAG, NotePayload(id=result.value.id, tags={"new tag"}))
+    assert db.select(entity=Entities.NOTES, key=result.value.id).tags == {"new tag"}
+
+
+def test_delete_tags(setup_db):
+    result = controller(Actions.ADD_NOTE, NotePayload(value="test note"))
+    controller(Actions.ADD_TAG, NotePayload(id=result.value.id, tags={"test tag"}))
+    controller(Actions.DELETE_TAG, NotePayload(id=result.value.id))
+    assert db.select(entity=Entities.NOTES, key=result.value.id).tags == set()
+
+
+def test_find_by_tag(setup_db):
+    result = controller(Actions.ADD_NOTE, NotePayload(value="test note"))
+    controller(
+        Actions.ADD_TAG, NotePayload(id=result.value.id, tags={"test find by tag"})
+    )
+    result2 = controller(Actions.ADD_NOTE, NotePayload(value="test note2"))
+    controller(
+        Actions.ADD_TAG, NotePayload(id=result2.value.id, tags={"test find by tag"})
+    )
+    assert (
+        len(
+            controller(
+                Actions.FIND_BY_TAG, NotePayload(tags={"test find by tag"})
+            ).value
+        )
+        == 2
+    )
